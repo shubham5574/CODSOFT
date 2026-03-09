ab9b55e6ec65571d2e084c7935f5a721305d33d7
shubham5574 <shubhamkushwaha38001@gmail.com>
2026-03-09 15:30:41 +0530

    Add Portfolio, Landing Page, Calculator Projects

---
+++ portfolio/script.js
@@ -0,0 +1,9 @@
+const toggle = document.getElementById("toggle");
+const menu = document.getElementById("menu");
+
+toggle.addEventListener("click", () => {
+    menu.classList.toggle("show");
+});
+
+
+document.getElementById("year").textContent = new Date().getFullYear();
